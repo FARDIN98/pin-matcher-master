88643e3096dc2e764a54ce97a6e037f762be269c
Fardin Ahmed <fardinahmed66@gmail.com>
2021-02-01 14:44:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+// Display Pin
+function generatePin(){
+    const pinInput = document.getElementById("input-field");
+    pinInput.value = getPin();
+}
+
+// Handle Calculator Button
+const buttonContainer = document.getElementById("digit-parent");
+buttonContainer.addEventListener("click", function(event){
+    const digit = event.target.innerText;
+    if(isNaN(digit)){
+        //handle backspace
+        //handle clear
+    }
+    if(digit === 'C' ){
+        const typedInput = document.getElementById("typed-pin");
+        typedInput.value = '';
+    }
+    else{
+        const typedInput = document.getElementById("typed-pin");
+        typedInput.value = typedInput.value + digit;
+    }
+})
+
+//Verify Pin Number
+function verifyPin(){
+    const pin = document.getElementById("input-field").value;
+    const typedPin = document.getElementById("typed-pin").value;
+    if(pin === typedPin){
+        const correct = document.getElementById("correct-pin");
+        correct.style.display = "block";
+        const inCorrect = document.getElementById("incorrect-pin");
+        inCorrect.style.display = "none";
+    }
+    else{
+        const inCorrect = document.getElementById("incorrect-pin");
+        inCorrect.style.display = "none";
+        const correct = document.getElementById("correct-pin");
+        correct.style.display = "block";
+    }
+}
+
+
+
+
+
+
+
+
+function getPin(){
+    const random = Math.random()*10000;
+    const pin = (random + '').split('.')[0];
+    if(pin.length === 4){
+        return pin;
+    }
+    else{
+        return getPin();
+    }
+}
